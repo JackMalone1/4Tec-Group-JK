e4ca51c50cd3192a079c619ac23380bbdf22bb62
KiernenIrons <c00241492@itcarlow.ie>
2021-11-26 09:40:02 +0000

    Initial Commit

---
+++ 4Tec-Group-JK/4Tec-Group-JK/Game.h
@@ -0,0 +1,44 @@
+/// <summary>
+/// author Pete Lowe May 2019
+/// you need to change the above line or lose marks
+/// </summary>
+#ifndef GAME_HPP
+#define GAME_HPP
+/// <summary>
+/// include guards used so we don't process this file twice
+/// same as #pragma once
+/// Don't forget the endif at the bottom
+/// </summary>
+#include <SFML/Graphics.hpp>
+
+class Game
+{
+public:
+	Game();
+	~Game();
+	/// <summary>
+	/// main method for game
+	/// </summary>
+	void run();
+
+private:
+
+	void processEvents();
+	void processKeys(sf::Event t_event);
+	void update(sf::Time t_deltaTime);
+	void render();
+	
+	void setupFontAndText();
+	void setupSprite();
+
+	sf::RenderWindow m_window; // main SFML window
+	sf::Font m_ArialBlackfont; // font used by message
+	sf::Text m_welcomeMessage; // text used for message on screen
+	sf::Texture m_logoTexture; // texture used for sfml logo
+	sf::Sprite m_logoSprite; // sprite used for sfml logo
+	bool m_exitGame; // control exiting game
+
+};
+
+#endif // !GAME_HPP
+
